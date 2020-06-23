eecac9c63d9fbf5198f858f7e5d99046b393e1d0
P4radigm <leon.vanoldenborgh@student.hku.nl>
2020-06-23 19:21:21 +0200

    Finished TargetVisualController

--- Mimont/Assets/Scripts/Visuals/TargetVisualController.cs
+++ Mimont/Assets/Scripts/Visuals/TargetVisualController.cs
@@ -6,40 +6,116 @@ using UnityEngine.InputSystem;
 
 public class TargetVisualController : MonoBehaviour
 {
-    [SerializeField] private float VFXdelay;
-    [SerializeField] private float addedScale;
+    [Header("Main Options")]
+    [ColorUsage(true, true)] public Color targetColor;
+    public float startScale;
+    public float maxScale;
+    public float addedScale;
+
+    [Header("Growth Options")]
+    [SerializeField] private float burstDelay;
     [SerializeField] private AnimationCurve growCurve;
     [SerializeField] private float growDuration;
-    [SerializeField, ColorUsage(true, true)] private Color targetColor;
-    [Space]
-    [SerializeField] private VisualEffect burst;
+
+    [Header("Spawn Options")]
+    [SerializeField] private float orbFadeInDuration;
+    [SerializeField] private AnimationCurve orbFadeInCurve;
+
+    [Header("Hit Options")]
+    [SerializeField] private float orbFadeGrowDuration;
+    [SerializeField] private float orbFadeShrinkDuration;
+    [SerializeField] private AnimationCurve orbFadeOutCurve;
+    [SerializeField] private AnimationCurve orbFadeGrowCurve;
+    [SerializeField] private AnimationCurve orbFadeShrinkCurve;
+
+    [Header("Needed")]
+    [SerializeField] private VisualEffect burstOut;
+    [SerializeField] private VisualEffect burstIn;
     [SerializeField] private VisualEffect constant;
+
     private Material targetMat;
 
+    private Coroutine spawnRoutine;
     private Coroutine growRoutine;
+    private Coroutine hitRoutine;
 
-    // Start is called before the first frame update
     void Start()
     {
         targetMat = GetComponent<Renderer>().material;
 
+        //Set color
         targetMat.SetColor("_FresnelColor", targetColor);
-        burst.SetVector4("Color", targetColor);
+        burstOut.SetVector4("Color", targetColor);
+        burstIn.SetVector4("Color", targetColor);
         constant.SetVector4("Color", targetColor);
-        burst.Stop();
-        constant.Play();
+
+        //Idk waarom dit nodig is, maar ja fucking VFX graph
+        burstOut.Stop();
+        burstIn.Stop();
+        constant.Stop();
+        constant.enabled = false;
+        burstIn.enabled = false;
+        burstOut.enabled = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        if (Keyboard.current.bKey.wasPressedThisFrame)
+        {
+            StartSpawn();
+        }
+
         if (Keyboard.current.nKey.wasPressedThisFrame)
         {
             StartGrow();
         }
+
+        if (Keyboard.current.mKey.wasPressedThisFrame)
+        {
+            StartHit();
+        }
     }
 
-    private void StartGrow()
+    #region spawn
+    public void StartSpawn()
+    {
+        if (spawnRoutine != null) { return; }
+        spawnRoutine = StartCoroutine(SpawnIE());
+    }
+
+    private IEnumerator SpawnIE()
+    {
+        constant.enabled = true;
+        burstIn.enabled = true;
+        burstOut.enabled = true;
+        transform.localScale = new Vector3(startScale, startScale, startScale);
+
+        burstOut.SetFloat("Count", 12f);
+
+        constant.Play();
+        burstOut.Play();
+        yield return new WaitForSeconds(burstDelay);
+
+        float _timeValue = 0;
+
+        while (_timeValue < 1)
+        {
+            _timeValue += Time.deltaTime / orbFadeInDuration;
+            float _evaluatedTimeValue = orbFadeInCurve.Evaluate(_timeValue);
+            float _newOpacity = Mathf.Lerp(0f, 1f, _evaluatedTimeValue);
+
+            targetMat.SetFloat("_Opacity", _newOpacity);
+
+            yield return null;
+        }
+
+        spawnRoutine = null;
+        yield return null;
+    }
+    #endregion
+
+    #region Grow
+    public void StartGrow()
     {
         if(growRoutine != null) { return; }
         growRoutine = StartCoroutine(GrowIE());
@@ -47,27 +123,101 @@ public class TargetVisualController : MonoBehaviour
 
     private IEnumerator GrowIE()
     {
-        burst.Play();
-        yield return new WaitForSeconds(VFXdelay);
+        burstOut.Play();
+        burstOut.SetFloat("Count", 6);
+        burstOut.SetFloat("LifetimeTrail", 1.1f);
+        burstOut.SetFloat("Size", 0.03f);
+        yield return new WaitForSeconds(burstDelay);
 
-        float _timeValue = 0;
-
-        Vector3 _oldSize = transform.localScale;
-        Vector3 _newSize = transform.localScale + new Vector3(addedScale, addedScale, addedScale);
-
-        while (_timeValue < 1)
+        if(transform.localScale.x < maxScale)
         {
-            _timeValue += Time.deltaTime / growDuration;
-            float _evaluatedTimeValue = growCurve.Evaluate(_timeValue);
-            Vector3 _Size = Vector3.Lerp(_oldSize, _newSize, _evaluatedTimeValue);
+            float _timeValue = 0;
 
-            transform.localScale = _Size;
+            Vector3 _oldSize = transform.localScale;
+            Vector3 _newSize = transform.localScale + new Vector3(addedScale, addedScale, addedScale);
 
-            yield return null;
+            while (_timeValue < 1)
+            {
+                _timeValue += Time.deltaTime / growDuration;
+                float _evaluatedTimeValue = growCurve.Evaluate(_timeValue);
+                Vector3 _Size = Vector3.Lerp(_oldSize, _newSize, _evaluatedTimeValue);
+
+                transform.localScale = _Size;
+
+                yield return null;
+            }
         }
+        else
+        {
+            GetComponent<Renderer>().enabled = false;
+            constant.Stop();
+            //disable collider
+            yield return new WaitForSeconds(4f);
+            Destroy(gameObject);
+        }
+
+
 
 
         growRoutine = null;
         yield return null;
     }
+    #endregion
+
+    #region Hit
+
+    public void StartHit()
+    {
+        if(hitRoutine != null) { return; }
+        hitRoutine = StartCoroutine(HitIE());
+    }
+
+    private IEnumerator HitIE()
+    {
+
+        float _growTimeValue = 0;
+
+        float _startScale = targetMat.GetFloat("_ScaleOffset");
+
+
+        while (_growTimeValue < 1)
+        {
+            _growTimeValue += Time.deltaTime / orbFadeGrowDuration;
+            float _evaluatedTimeValue = orbFadeGrowCurve.Evaluate(_growTimeValue);
+            float _newScale = Mathf.Lerp(_startScale, _startScale + 0.1f, _evaluatedTimeValue);
+
+            targetMat.SetFloat("_ScaleOffset", _newScale);
+
+            yield return null;
+        }
+
+        float _shrinkTimeValue = 0;
+
+        burstIn.Play();
+
+        while (_shrinkTimeValue < 1)
+        {
+            _shrinkTimeValue += Time.deltaTime / orbFadeShrinkDuration;
+            float _evaluatedTimeShrink = orbFadeShrinkCurve.Evaluate(_shrinkTimeValue);
+            float _evaluatedTimeOpacity = orbFadeOutCurve.Evaluate(_shrinkTimeValue);
+            float _newScale = Mathf.Lerp(_startScale + 0.1f, _startScale - 0.05f, _evaluatedTimeShrink);
+            targetMat.SetFloat("_ScaleOffset", _newScale);
+            float _newOpacity = Mathf.Lerp(1f, 0f, _evaluatedTimeOpacity);
+            targetMat.SetFloat("_Opacity", _newOpacity);
+
+
+            yield return null;
+        }
+
+        constant.Stop();
+
+        yield return new WaitForSeconds(5f);
+
+        Destroy(gameObject);
+
+        hitRoutine = null;
+        yield return null;
+    }
+
+    #endregion
 }
