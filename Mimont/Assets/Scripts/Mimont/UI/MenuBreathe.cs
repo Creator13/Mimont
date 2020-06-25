79d48c63a6693acdbbd3f8bfe3e359954654c0e8
P4radigm <leon.vanoldenborgh@student.hku.nl>
2020-06-25 16:56:33 +0200

    Menu breather

---
+++ Mimont/Assets/Scripts/Mimont/UI/MenuBreathe.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class MenuBreathe : MonoBehaviour
+{
+    [SerializeField] private float minBloom;
+    [SerializeField] private float maxBloom;
+    [SerializeField] private float baseBloom;
+    [SerializeField] private float breatheSpeed;
+
+    private VolumeProfile v;
+    private Bloom b;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        v = GameObject.FindObjectOfType<Volume>().profile;
+        v.TryGet(out b);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        b.intensity.value = Mathf.Lerp(minBloom, maxBloom, Mathf.Sin(Time.time * breatheSpeed));
+    }
+
+    public void ResetBloom()
+    {
+        b.intensity.value = baseBloom;
+    }
+}
