7eb6fb31e5ebcf30baeae0f16200cc2344fa639e
P4radigm <leon.vanoldenborgh@student.hku.nl>
2020-06-28 18:42:46 +0200

    Job Balancing

--- Mimont/Assets/Scripts/Mimont/JobMode/JobSprite.cs
+++ Mimont/Assets/Scripts/Mimont/JobMode/JobSprite.cs
@@ -7,16 +7,27 @@ public class JobSprite : MonoBehaviour {
     [SerializeField] private Sprite kiss1;
     [SerializeField] private Sprite kiss2;
 
+        [SerializeField] private float idleShrink = 0.8f;
+
+    private Vector3 initialScale;
     private new SpriteRenderer renderer;
     private SpriteRenderer Renderer => renderer ? renderer : renderer = GetComponent<SpriteRenderer>();
 
-    public void SetIdle() {
+        private void Awake()
+        {
+            initialScale = transform.localScale;
+        }
+
+        public void SetIdle() {
         Renderer.sprite = Random.value < .5f ? idle1 : idle2;
+            transform.localScale = initialScale * idleShrink;
+
     }
 
     public void SetKiss() {
         Renderer.sprite = Random.value < .5f ? kiss1 : kiss2;
-    }
+            transform.localScale = initialScale;
+        }
 
     private void OnValidate() {
         if (idle1) {
